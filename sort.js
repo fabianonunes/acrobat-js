5026e19eccab9df7708aecba71cf84ab3e191963
Fabiano Nunes <fabiano@nunes.me>
2011-11-13 12:07:06 -0200

    remove duplicates bkm

--- sort.js
+++ sort.js
@@ -6,23 +6,27 @@ function sortByPageNumber(root) {
 		, chd = root.children
 		, stack = []
 		, names = {}
-		, key, value, pageNames;
+		, key, bkm;
 
 	if(!chd) { return; }
 
-	for(key = -1; value = chd[++key];){
-		value.execute();
-		value.pageNum = this.pageNum;		
-		stack.push(value);
-		value.children && sortByPageNumber(value);
+	for(key = -1; bkm = chd[++key];){
+		bkm.execute();
+		bkm.pageNum = this.pageNum;		
+		stack.push(bkm);
+		bkm.children && sortByPageNumber(bkm);
+		names[bkm.pageNum] = {};
 	}
 
 	stack.sort(function(a, b){ return a.pageNum - b.pageNum; });
 
-	for(key = -1; value = stack[++key];){
-		names[value.pageNum] || (pageNames = names[value.pageNum] = {});
-		pageNames[value.name] || root.insertChild(value, chd.length);
-		pageNames[value.name] = true;
+	for(key = -1; bkm = stack[++key];){
+		if(names[bkm.pageNum][bkm.name] && !bkm.children){
+			bkm.remove();
+		}  else {
+			root.insertChild(bkm, chd.length);
+		}
+		names[bkm.pageNum][bkm.name] = true;
 	}
 
 	this.pageNum = returnTo;
