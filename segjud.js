28911056a2bf37b66fc31ee37d5cb13799b36a44
Fabiano Nunes <fabiano@nunes.me>
2012-03-20 17:45:20 -0300

    segjud config

--- segjud.js
+++ segjud.js
@@ -1,39 +1,27 @@
-/*global app*/
-var ConfigJs = {
+/*global app, AcrobatJs, _*/
+
+var _config = {
 	paths : {
 		7 : '/p/status_7.pdf',
-		8 : '/p/status_7.pdf'
+		8 : '/p/status_8.pdf'
+	},
+	commands : [ 'cpe', 'ccadp', 'query', 'esij' ],
+	tools : {
+		cpe : {
+			cLabel : "Certid\u00e3o - CPE",
+			cExec : "AcrobatJs.createCert(8, _config.paths[8])"
+		},
+		ccadp : {
+			cLabel : "Certid\u00e3o - CCADP",
+			cExec : "AcrobatJs.addCert(7, _config.paths[7])"
+		},
+		query : {
+			cLabel : "Consulta Processual",
+			cExec : "AcrobatJs.launchQuery()"
+		},
+		esij : {
+			cLabel : "e-SIJ",
+			cExec : "AcrobatJs.launchEsij()"
+		}
 	}
 }
-
-app.addToolButton({
-	cName: "launchQueryButton",
-	cLabel: "Consulta Processual",
-	cEnable: "event.rc = (event.target != null)",
-	cExec: "AcrobatJs.launchQuery()",
-	nPos: -1
-})
-
-app.addToolButton({
-	cName: "launchEsijButton",
-	cLabel: "e-SIJ",
-	cEnable: "event.rc = (event.target != null)",
-	cExec: "AcrobatJs.launchEsij()",
-	nPos: -1
-})
-
-app.addToolButton({
-	cName: "addCert7Button",
-	cLabel: "Adicionar Certid\u00e3o - CCADP",
-	cEnable: "event.rc = (event.target != null)",
-	cExec: "AcrobatJs.addCert(7, ConfigJs.paths[7])",
-	nPos: -1
-})
-
-app.addToolButton({
-	cName: "createCertButton",
-	cLabel: "Criar certid\u00e3o - CPE",
-	cEnable: "event.rc = (event.target != null)",
-	cExec: "AcrobatJs.createCert(8, ConfigJs.paths[8])",
-	nPos: -1
-})
